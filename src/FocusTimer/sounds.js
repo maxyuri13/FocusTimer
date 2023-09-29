0aa0e300a7944afe53867323cc0d6bbcd694e993
Max Yuri <91703339+maxyuri13@users.noreply.github.com>
2023-09-29 19:02:27 -0300

    Add files via upload

---
+++ src/FocusTimer/sounds.js
@@ -0,0 +1,5 @@
+export const buttonPressAudio = new Audio('./Assets/button-press.wav')
+export const kitchenTimer = new Audio('./Assets/kichen-timer.mp3')
+export const bgAudio = new Audio('./Assets/bg-audio.mp3')
+
+bgAudio.loop = true 
